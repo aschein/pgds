69b0db9a2d21712456288a5ea7f3e1cb1c052134
Aaron Schein <scheinaj@gmail.com>
2016-10-22 13:35:34 -0400

    collect results

---
+++ src/collect_results.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import numpy as np
+
+from glob import glob
+from path import path
+
+from collections import defaultdict
+
+RESULTS_DIR = path('/mnt/nfs/work1/wallach/aschein/results/NIPS16/camera_ready')
+
+
+def foo(out_dir, pattern='smoothing_eval.txt'):
+    """
+    Finds all results files in a directory and its sub-directories.
+
+    Returns mean and std across each file for each error metric.
+    """
+    results = defaultdict(list)
+
+    for eval_file in glob(pattern):
+        with open(eval_file) as f:
+            lines = [line.rstrip() for line in f.readlines()]
+            header = lines[0]
+            eval_lines = lines[1:]
+
+        col_names = header.split()
+        assert col_names[0] == 'ITN'
+        metric_names = col_names[1:]
+
+        for line in eval_lines:
+            metric_vals = [float(m) for m in line.split()[1:]]
+            for k, v in zip(metric_names, metric_vals):
+                results[k].append(v)
+
+    print out_dir
+    for k, v in results.iteritems():
+        print '%s: %f +- %f' % (k, np.mean(v), np.std(v))
+
+
+if __name__ == '__main__':
+    foo(RESULTS_DIR.joinpath('icews/undirected/2001-D/*/K_10/lds/smoothing_eval.txt'))
