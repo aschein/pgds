e9cae800136ffafde3be2b86139f3878889db377
Aaron Schein <scheinaj@gmail.com>
2016-10-21 20:42:52 -0400

    changing setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,23 @@
+# import numpy as np
+# from Cython.Build import cythonize
+# from distutils.core import setup
+
+
+# setup(
+#     include_dirs=[np.get_include()],
+#     ext_modules=cythonize(['**/*.pyx'])
+# )
+
+#!/usr/bin/env pythonv
 import numpy as np
-from Cython.Build import cythonize
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 
 setup(
-    include_dirs=[np.get_include()],
-    ext_modules=cythonize(['**/*.pyx'])
+    cmdclass={'build_ext': build_ext},
+    include_dirs=[np.get_include(), '../../fatwalrus'],
+    ext_modules=cythonize('**/*.pyx', include_path=['../../fatwalrus'])
 )
