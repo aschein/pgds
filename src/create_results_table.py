4fcea308442ea17a17048c89dcacb957abda1cb0
Aaron Schein <scheinaj@gmail.com>
2016-10-23 17:19:22 -0400

    adding table script

--- src/create_results_table.py
+++ src/create_results_table.py
@@ -52,7 +52,7 @@ def foo():
             print 'MODEL\tMRE\t\tMAE\t\tRMSE'
             for version in ['pgds', 'gpdpfa', 'lds']:
                 K = 25 if version == 'lds' else 100
-                pattern = dataset_dir.joinpath('*/masked_subset_[3|4]/K_%d/%s' % (K, version))
+                pattern = dataset_dir.joinpath('*/masked_subset_[5|6]/K_%d/%s' % (K, version))
                 pattern = pattern.joinpath('avg_%s_eval.txt' % pred_type)
                 results = get_averaged_results(pattern)
                 print '%s\t%f\t%f\t%f' % (version,
