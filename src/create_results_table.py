b3c5cae3142f3c2f257ab7beb926100718f10786
Aaron Schein <scheinaj@gmail.com>
2016-10-23 17:35:29 -0400

    adding table script

--- src/create_results_table.py
+++ src/create_results_table.py
@@ -26,6 +26,7 @@ def get_averaged_results(pattern='avg_smoothing_eval.txt'):
     results = defaultdict(list)
 
     for eval_file in glob(pattern):
+        print eval_file
         with open(eval_file) as f:
             lines = [line.rstrip() for line in f.readlines()]
             header = lines[0]
