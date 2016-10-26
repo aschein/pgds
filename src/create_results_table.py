4063f8027d46d960e3e3861a8402abe41abb14cd
Aaron Schein <scheinaj@gmail.com>
2016-10-26 19:56:47 -0400

    checking full data

--- src/create_results_table.py
+++ src/create_results_table.py
@@ -111,7 +111,7 @@ def print_latex_table():
             for version in ['pgds', 'gpdpfa', 'lds']:
                 K = 25 if version == 'lds' else 100
 
-                path_str = 'masked_subset_5/K_%d/%s' % (K, version)
+                path_str = 'masked_subset_[3|4]/K_%d/%s' % (K, version)
                 if 'gdelt' in data_name or 'icews' in data_name:
                     path_str = '*/' + path_str
                 pattern = dataset_dir.joinpath(path_str)
